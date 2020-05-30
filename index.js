f01d446f6b15147fc36ff0f27338499a703102ce
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-30 02:54:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,7 @@ function unleashDog(dogName, dogBreed) {
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+  return routine.each()
+}
