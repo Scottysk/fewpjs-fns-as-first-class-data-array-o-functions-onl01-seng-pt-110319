e724431e30a1787b10e280117f16f72e98167acc
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-30 02:48:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`;
 }
+
+function walkToPark(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`;
+}
