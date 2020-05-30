378fea29f874bd8ee358a9c2c00d0d89f00d3227
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-30 02:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`;
+}
